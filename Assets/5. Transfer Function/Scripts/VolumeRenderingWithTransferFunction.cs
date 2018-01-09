1af5d286c7821d25b01b763a34e21fcad39108af
hecomi <info@hecomi.com>
2018-01-10 00:10:05 +0900

    add transfer function example.

---
+++ Assets/5. Transfer Function/Scripts/VolumeRenderingWithTransferFunction.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class VolumeRenderingWithTransferFunction : MonoBehaviour
+{
+    const int width = 100;
+
+	[SerializeField]
+	Gradient gradient;
+
+    Texture2D texture_;
+
+    void Start()
+    {
+        UpdateTexture();
+    }
+
+    [ContextMenu("UpdateTexture")]
+    void UpdateTexture()
+    {
+        texture_ = new Texture2D(100, 1, TextureFormat.ARGB32, false);
+        for (int i = 0; i < width; ++i)
+        {
+            var t = (float)i / width;
+            texture_.SetPixel(i, 0, gradient.Evaluate(t));
+        }
+        texture_.Apply(false);
+        var renderer = GetComponent<Renderer>();
+        renderer.sharedMaterial.SetTexture("_Transfer", texture_);
+    }
+}
